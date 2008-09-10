b99c38a27ef7abf03a809d2661ac5d43cc726c8f
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-09-10 15:01:08 +0000

    masak: you have to implement IContentOutlinePage instead of extend ContentOutlinePage
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6933 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.biojava.ui/src/net/bioclipse/biojava/ui/views/outline/AlignmentOutlinePage.java
+++ plugins/net.bioclipse.biojava.ui/src/net/bioclipse/biojava/ui/views/outline/AlignmentOutlinePage.java
@@ -4,21 +4,27 @@ import net.bioclipse.biojava.ui.editors.AlignmentEditor;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.part.Page;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
+import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 
-public class AlignmentOutlinePage extends ContentOutlinePage
-                                  implements ISelectionListener, IAdaptable {
+public class AlignmentOutlinePage extends Page
+                                  implements IContentOutlinePage,
+                                  ISelectionListener, IAdaptable {
 
     private IEditorInput input;
+    private Canvas sequenceCanvas;
 
     public AlignmentOutlinePage(IEditorInput input, AlignmentEditor editor) {
         super();
@@ -27,11 +33,13 @@ public class AlignmentOutlinePage extends ContentOutlinePage
     }
     
     public void createControl(Composite parent) {
-        final Canvas sequenceCanvas = new Canvas( parent, SWT.NONE );
+        
+        sequenceCanvas = new Canvas( parent, SWT.NONE );
         sequenceCanvas.setLocation( 0, 0 );
         sequenceCanvas.addPaintListener( new PaintListener() {
             public void paintControl(PaintEvent e) {
                 GC gc = e.gc;
+                gc.setBackground( gc.getDevice().getSystemColor( SWT.COLOR_CYAN ) );
                 gc.fillRectangle(0, 0, 40, 40);
             }
         } );
@@ -42,6 +50,44 @@ public class AlignmentOutlinePage extends ContentOutlinePage
 
     public Object getAdapter( Class adapter ) {
         return null;
+    }
+
+    @Override
+    public Control getControl() {
+        
+        return sequenceCanvas;
+    }
+
+    @Override
+    public void setFocus() {
+
+        sequenceCanvas.setFocus();
+        
+    }
+
+    public void addSelectionChangedListener( ISelectionChangedListener listener ) {
+
+       
+        
+    }
+
+    public ISelection getSelection() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void removeSelectionChangedListener(
+                                                ISelectionChangedListener listener ) {
+
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setSelection( ISelection selection ) {
+
+        // TODO Auto-generated method stub
+        
     }
     
 }
