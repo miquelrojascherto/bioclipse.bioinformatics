350d730a7b3e4523193f0d6b5485e155a111a77d
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-30 14:49:26 +0000

    fixed javadoc comments, added @PublishedClass and @PublishedMethod annotations. much work remains to be done.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8104 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.biojava.business/src/net/bioclipse/biojava/business/IBiojavaManager.java
+++ plugins/net.bioclipse.biojava.business/src/net/bioclipse/biojava/business/IBiojavaManager.java
@@ -18,15 +18,18 @@ import net.bioclipse.biojava.domain.BiojavaDNASequence;
 import net.bioclipse.biojava.domain.BiojavaRNASequence;
 import net.bioclipse.biojava.domain.BiojavaSequence;
 import net.bioclipse.biojava.domain.BiojavaSequenceList;
+import net.bioclipse.core.PublishedClass;
+import net.bioclipse.core.PublishedMethod;
 import net.bioclipse.core.Recorded;
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.business.IBioclipseManager;
 /**
  * @author jonalv, ola
  */
+@PublishedClass("Provides bioinformatics services through the BioJava project.")
 public interface IBiojavaManager extends IBioclipseManager {
     /**
-     * Enum over the sequence formats supported by the Biojavamanager
+     * Enum over the sequence formats supported by the BiojavaManager.
      *
      * @author jonalv
      *
@@ -38,7 +41,7 @@ public interface IBiojavaManager extends IBioclipseManager {
         UNIPROT;
     }
     /**
-     * Converts a BiojavaSequence to a String representation in a given format
+     * Converts a BiojavaSequence to a String representation in a given format.
      *
      * @param format the format of the resulting string
      * @param seq the sequence to convert
@@ -47,111 +50,150 @@ public interface IBiojavaManager extends IBioclipseManager {
      * @throws ScriptingException
      */
     @Recorded
+    @PublishedMethod(params="SequenceFormat format",
+    		     methodSummary="Converts a BiojavaSequence to a String "
+                         + "representation in a given format.")
     public String renderAs( SequenceFormat format,
                             BiojavaSequence seq) throws IOException;
     //TODO renderAs giving a stream?
     /**
-     * Loads a BiojavaSequence from file
+     * Loads a BiojavaSequence from file.
      *
      * @param path The path to the file
      * @return a BioJavaSequence object
      * @throws IOException
      */
     @Recorded
+    @PublishedMethod(params="String path",
+                     methodSummary="Loads a BiojavaSequence from file.")
     public BiojavaSequence loadSequence( String path ) throws IOException;
      /**
-     * Load sequenceCollection from InputStream
+      * Loads sequenceCollection from InputStream.
+      * 
       * @param instream the Stream to read from
       * @return A BiojavaSequenceCollection object
-     * @throws BioclipseException
+      * @throws BioclipseException
       */
     @Recorded
+    @PublishedMethod(params="InputStream instream",
+                     methodSummary="Loads sequenceCollection from InputStream.")
     public BiojavaSequenceList loadSequences(InputStream instream) throws BioclipseException;
      /**
-     * Load sequenceCollection from InputStream
+      * Loads sequenceCollection from InputStream.
+      * 
       * @param instream the Stream to read from
       * @return A BiojavaSequenceCollection object
-     * @throws BioclipseException
+      * @throws BioclipseException
       */
     @Recorded
+    @PublishedMethod(params="String path",
+                     methodSummary="Loads sequenceCollection from InputStream.")
     public BiojavaSequenceList loadSequences( String path ) throws IOException, BioclipseException;
     /**
      * Saves a Biojava sequence to the file it is connected to using the format
-     * of the file it is connected to
+     * of the file it is connected to.
      *
      * @param seq Biojava Sequence to save
      * @throws IllegalStateException if the sequence not connected to a file
      */
     @Recorded
+    @PublishedMethod(params="BiojavaSequence seq",
+                     methodSummary="Saves a Biojava sequence to the file it is connected to using the format of the file it is connected to.")
     public void saveSequence(BiojavaSequence seq) throws IllegalStateException;
     /**
-     * Saves a Biojava sequence to a given path using the default format
+     * Saves a Biojava sequence to a given path using the default format.
      *
      * @param seq Sequence to save
      * @param fileName name of file to save to
      */
     @Recorded
+    @PublishedMethod(params="BiojavaSequence seq, String path",
+                     methodSummary="Saves a Biojava sequence to a given path using the default format.")
     public void saveSequence( BiojavaSequence seq, String path );
     /**
-     * Saves a Biojava sequence to a given path using a given format
+     * Saves a Biojava sequence to a given path using a given format.
      *
      * @param seq
      * @param format
      * @param path
      */
     @Recorded
+    @PublishedMethod(params="BiojavaSequence seq, String path, SequenceFormat format",
+                     methodSummary="Saves a Biojava sequence to a given path using a given format.")
     public void saveSequence( BiojavaSequence seq,
                               String path,
                               SequenceFormat format );
     /**
-     * Create sequence from a plain sequence string. Will set name to empty.
+     * Creates sequence from a plain sequence string. Will set name to empty.
+     * 
      * @throws ScriptingException
      */
     @Recorded
+    @PublishedMethod(params="String content",
+                     methodSummary="Creates sequence from a plain sequence string. Will set name to empty.")
     public BiojavaSequence createSequence(String content);
     /**
-     * Create sequence from a plain sequence string. Will set name to empty.
+     * Creates sequence from a plain sequence string. Will set name to empty.
      */
     @Recorded
+    @PublishedMethod(params="String content",
+                     methodSummary="Creates sequence from a plain sequence string. Will set name to empty.")
     public BiojavaSequence createSequenceFromFasta(String content);
     /**
-     * Create sequence from a name and a plain sequence string (content).
+     * Creates sequence from a name and a plain sequence string (content).
      */
     @Recorded
+    @PublishedMethod(params="String name, String content",
+                     methodSummary="Creates sequence from a name and a plain sequence string (content).")
     public BiojavaSequence createSequence(String name, String content);
     /**
-     * Load sequence from InputStream
+     * Loads sequence from InputStream.
+     * 
      * @param instream to be loaded
      * @return loaded sequence
      */
     @Recorded
+    @PublishedMethod(params="InputStream instream",
+                     methodSummary="Loads sequence from InputStream.")
     public BiojavaSequence loadSequence(InputStream instream);
     /**
-     * Converts a DNA sequence into RNA (transcription)
+     * Converts a DNA sequence into RNA (transcription).
      *
      * @param sequence to be converted
      * @return an RNA sequence
      */
     @Recorded
+    @PublishedMethod(params="BiojavaDNASequence sequence",
+                     methodSummary="Converts a DNA sequence into RNA (transcription).")
     public BiojavaRNASequence DNAtoRNA( BiojavaDNASequence sequence );
     /**
-     * Convert a BiojavaDNASequence into a BiojavaAASequence. Trancription + translation
+     * Converts a BiojavaDNASequence into a BiojavaAASequence.
+     * Trancription + translation.
+     * 
      * @throws IllegalArgumentException
      */
     @Recorded
+    @PublishedMethod(params="BiojavaDNASequence sequence",
+                     methodSummary="Converts a BiojavaDNASequence into a BiojavaAASequence.")
     public BiojavaAASequence DNAToProtein(BiojavaDNASequence sequence);
     /**
-     * Converts a RNA sequence into DNA (reverse transcription)
+     * Converts a RNA sequence into DNA (reverse transcription).
      *
      * @param sequence to be converted
      * @return an DNA sequence
      */
     @Recorded
+    @PublishedMethod(params="BiojavaRNASequence rnaSeq",
+                     methodSummary="Converts a RNA sequence into DNA (reverse transcription).")
     public BiojavaDNASequence RNAtoDNA(BiojavaRNASequence rnaSeq);
     /**
-     * Convert a BiojavaRNASequence into a BiojavaAASequence. Biological meaning: Translation.
+     * Converts a BiojavaRNASequence into a BiojavaAASequence.
+     * Biological meaning: Translation.
+     * 
      * @throws IllegalArgumentException
      */
     @Recorded
+    @PublishedMethod(params="BiojavaRNASequence sequence",
+                     methodSummary="Convert a BiojavaRNASequence into a BiojavaAASequence.")
     public BiojavaAASequence RNAtoProtein(BiojavaRNASequence sequence);
 }
